daff7f3bf5ffcc140e827f3a3679036c1f23d58a
Kazimierz Parzych <2738006+kazek@users.noreply.github.com>
2026-04-21 19:23:06 +0200

    Bump version to 0.4.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.navilive.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
