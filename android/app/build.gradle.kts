0f75536e42245813c583b64d50f3cd2ee4e5bb0d
Kazimierz Parzych <2738006+kazek@users.noreply.github.com>
2026-04-23 01:13:00 +0200

    Align Android versioning to 1.0.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.navilive.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.6.4"
+        versionCode = 13
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
