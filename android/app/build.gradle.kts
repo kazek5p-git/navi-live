e583a26522167e9a43ae982e6eda58a0d81195f3
Kazimierz Parzych <2738006+kazek@users.noreply.github.com>
2026-05-07 23:45:54 +0200

    Bump Android version to 1.0.12

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.navilive.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.11"
+        versionCode = 24
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
